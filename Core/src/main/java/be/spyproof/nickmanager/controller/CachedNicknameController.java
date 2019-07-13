6df45c8513cc11a23022cdddfa0e856f04c7042b
Nils Yseboodt <nils.yseboodt@gmail.com>
2019-07-13 06:28:35 +0200

    (1.1.2) Fix: sometimes not finding a player

--- Core/src/main/java/be/spyproof/nickmanager/controller/CachedNicknameController.java
+++ Core/src/main/java/be/spyproof/nickmanager/controller/CachedNicknameController.java
@@ -10,123 +10,107 @@ import java.util.*;
 /**
  * Created by Spyproof on 28/10/2016.
  */
-public class CachedNicknameController extends NicknameController
-{
-    private Set<NicknameData> cachedPlayers;
+public class CachedNicknameController extends NicknameController {
+  private Set<NicknameData> cachedPlayers;
 
-    public CachedNicknameController(IPlayerStorage storage)
-    {
-        super(storage);
-        this.cachedPlayers = new HashSet<>();
-    }
+  public CachedNicknameController(IPlayerStorage storage) {
+    super(storage);
+    this.cachedPlayers = new HashSet<>();
+  }
 
-    @Override
-    public Optional<NicknameData> getPlayer(String name)
-    {
-        Optional<NicknameData> player = getCachedOrStoredPlayer(name);
+  @Override
+  public Optional<NicknameData> getPlayer(String name) {
+    return getCachedOrStoredPlayer(name);
+  }
 
-        if (player.isPresent() && !this.cachedPlayers.contains(player.get()))
-            this.cachedPlayers.add(player.get());
+  @Override
+  public Optional<NicknameData> getPlayer(UUID uuid) {
+    return getCachedOrStoredPlayer(uuid);
+  }
 
-        return player;
-    }
+  @Override
+  public void removePlayer(NicknameData player) {
+    super.removePlayer(player);
+    removeCachedPlayer(player.getUuid());
+  }
 
-    @Override
-    public Optional<NicknameData> getPlayer(UUID uuid)
-    {
-        Optional<NicknameData> player = getCachedOrStoredPlayer(uuid);
+  @Override
+  public void logout(UUID uuid) {
+    removeCachedPlayer(uuid);
+  }
 
-        if (player.isPresent() && !this.cachedPlayers.contains(player.get()))
-            this.cachedPlayers.add(player.get());
+  @Override
+  public void close() throws IOException {
+    this.storage.close();
+    this.cachedPlayers.clear();
+  }
 
-        return player;
-    }
+  protected void cachePlayer(NicknameData nicknameData) {
+    this.cachedPlayers.add(nicknameData);
+  }
 
-    @Override
-    public void removePlayer(NicknameData player)
-    {
-        super.removePlayer(player);
-        removeCachedPlayer(player.getUuid());
-    }
-
-    @Override
-    public void logout(UUID uuid)
-    {
-        removeCachedPlayer(uuid);
-    }
-
-    @Override
-    public void close() throws IOException
-    {
-        this.storage.close();
-        this.cachedPlayers.clear();
-    }
-
-    protected void cachePlayer(NicknameData nicknameData)
-    {
-        this.cachedPlayers.add(nicknameData);
-    }
-
-    protected void removeCachedPlayer(UUID... uuids)
-    {
-        Iterator<NicknameData> iterator = this.cachedPlayers.iterator();
-        while (iterator.hasNext())
-        {
-            NicknameData data = iterator.next();
-            for (UUID uuid : uuids)
-            {
-                if (data.getUuid().equals(uuid))
-                {
-                    iterator.remove();
-                    break;
-                }
-            }
+  protected void removeCachedPlayer(UUID... uuids) {
+    Iterator<NicknameData> iterator = this.cachedPlayers.iterator();
+    while (iterator.hasNext()) {
+      NicknameData data = iterator.next();
+      for (UUID uuid : uuids) {
+        if (data.getUuid().equals(uuid)) {
+          iterator.remove();
+          break;
         }
+      }
+    }
+  }
+
+  protected Optional<NicknameData> getCachedOrStoredPlayer(String name) {
+    Optional<NicknameData> player = getCachedPlayer(name);
+    if (player.isPresent()) {
+      return player;
     }
 
-    protected Optional<NicknameData> getCachedOrStoredPlayer(String name)
-    {
-        Optional<NicknameData> player = getCachedPlayer(name);
-        if (player.isPresent())
-            return player;
-
-        Optional<NicknameData> stored = super.getStoredPlayer(name);
-        if (stored.isPresent())
-            return stored;
-
-        return Optional.empty();
+    Optional<NicknameData> stored = super.getStoredPlayer(name);
+    if (stored.isPresent()) {
+      this.cachePlayer(stored.get());
+      return stored;
     }
 
-    protected Optional<NicknameData> getCachedOrStoredPlayer(UUID uuid)
-    {
-        Optional<NicknameData> player = getCachedPlayer(uuid);
-        if (player.isPresent())
-            return player;
+    return Optional.empty();
+  }
 
-        Optional<NicknameData> stored = super.getStoredPlayer(uuid);
-        if (stored.isPresent())
-            return stored;
-
-        return Optional.empty();
+  protected Optional<NicknameData> getCachedOrStoredPlayer(UUID uuid) {
+    Optional<NicknameData> player = getCachedPlayer(uuid);
+    if (player.isPresent()) {
+      return player;
     }
 
-    protected Optional<NicknameData> getCachedPlayer(String name)
-    {
-        for (NicknameData player : this.cachedPlayers)
-            if (player.getName().equalsIgnoreCase(name))
-                return Optional.of(player);
-
-        return Optional.empty();
+    Optional<NicknameData> stored = super.getStoredPlayer(uuid);
+    if (stored.isPresent()) {
+      this.cachePlayer(stored.get());
+      return stored;
     }
 
-    protected Optional<NicknameData> getCachedPlayer(UUID uuid)
-    {
-        for (NicknameData player : this.cachedPlayers)
-            if (player.getUuid().equals(uuid))
-                return Optional.of(player);
+    return Optional.empty();
+  }
 
-        return Optional.empty();
+  protected Optional<NicknameData> getCachedPlayer(String name) {
+    for (NicknameData player : this.cachedPlayers) {
+      if (player.getName().equalsIgnoreCase(name)) {
+        return Optional.of(player);
+      }
     }
+
+    return Optional.empty();
+  }
+
+  protected Optional<NicknameData> getCachedPlayer(UUID uuid) {
+    for (NicknameData player : this.cachedPlayers) {
+      if (player.getUuid().equals(uuid)) {
+        return Optional.of(player);
+      }
+    }
+
+    return Optional.empty();
+  }
 
 
 }
--- Core/src/main/java/be/spyproof/nickmanager/da/player/GsonPlayerStorage.java
+++ Core/src/main/java/be/spyproof/nickmanager/da/player/GsonPlayerStorage.java
@@ -12,232 +12,209 @@ import java.util.*;
 /**
  * Created by Spyproof on 03/11/2016.
  */
-public class GsonPlayerStorage implements IPlayerStorage
-{
-    private static final String EXTENSION = ".json";
-    private File playerDir;
+public class GsonPlayerStorage implements IPlayerStorage {
+  private static final String EXTENSION = ".json";
+  private File playerDir;
 
-    private Map<String, UUID> cachedPlayers = new TreeMap<>();
-    private Map<String, List<UUID>> cachedNicknames = new HashMap<>();
+  private Map<String, UUID> cachedPlayers = new TreeMap<>();
+  private Map<String, Set<UUID>> cachedNicknames = new HashMap<>();
 
-    public GsonPlayerStorage(File playerDir) throws IOException
-    {
-        this.playerDir = playerDir;
-        if (!playerDir.exists())
-            playerDir.mkdirs();
-        loadPlayerCache();
+  public GsonPlayerStorage(File playerDir) throws IOException {
+    this.playerDir = playerDir;
+    if (!playerDir.exists()) {
+      playerDir.mkdirs();
     }
+    loadPlayerCache();
+  }
 
-    private void loadPlayerCache() throws IOException
-    {
-        File[] playerFiles = playerDir.listFiles();
-        if (playerFiles == null)
-            return;
+  private void loadPlayerCache() throws IOException {
+    File[] playerFiles = playerDir.listFiles();
+    if (playerFiles == null)
+      return;
 
-        for (File file : playerFiles)
-        {
-            Optional<NicknameData> playerData = readPlayer(new JsonReader(new InputStreamReader(new FileInputStream(file), "UTF-8")), UUID.fromString(file.getName().replace(EXTENSION, "")));
-            if (playerData.isPresent())
-                addToCache(playerData.get());
+    for (File file : playerFiles) {
+      Optional<NicknameData> playerData = readPlayer(new JsonReader(new InputStreamReader(new FileInputStream(file), "UTF-8")), UUID.fromString(file.getName()
+                                                                                                                                                    .replace(EXTENSION, "")));
+      if (playerData.isPresent()) {
+        addToCache(playerData.get());
+      }
+    }
+  }
+
+  private void addToCache(NicknameData nicknameData) {
+    this.cachedPlayers.put(nicknameData.getName().toLowerCase(), nicknameData.getUuid());
+    if (nicknameData.getNickname().isPresent()) {
+      Set<UUID> set = this.cachedNicknames.get(nicknameData.getNickname().get());
+      if (set == null) {
+        set = new HashSet<>();
+        set.add(nicknameData.getUuid());
+        this.cachedNicknames.put(nicknameData.getNickname().get().replaceAll(Reference.COLOUR_AND_STYLE_PATTERN, "").toLowerCase(), set);
+      }
+      else {
+        set.add(nicknameData.getUuid());
+      }
+    }
+  }
+
+  @Override
+  public void savePlayer(NicknameData player) {
+    try {
+      writePlayer(player);
+      addToCache(player);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void removePlayer(NicknameData player) {
+    removePlayerFile(player.getUuid());
+  }
+
+  @Override
+  public Optional<NicknameData> getPlayer(String name) {
+    UUID uuid = this.cachedPlayers.get(name.toLowerCase());
+    if (uuid == null)
+      return Optional.empty();
+    else
+      return getPlayer(uuid);
+  }
+
+  @Override
+  public Optional<NicknameData> getPlayer(UUID uuid) {
+    try {
+      return readPlayer(uuid);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Optional.empty();
+    }
+  }
+
+  @Override
+  public List<NicknameData> getPlayerByNickname(String nickname, int limit) {
+    List<NicknameData> players = new ArrayList<>();
+    nickname = nickname.toLowerCase();
+
+    for (Map.Entry<String, Set<UUID>> entry : this.cachedNicknames.entrySet()) {
+      if (entry.getKey().toLowerCase().contains(nickname)) {
+        for (UUID uuid : entry.getValue()) {
+          Optional<NicknameData> playerData = getPlayer(uuid);
+          if (playerData.isPresent()) {
+            boolean isDuplicate = false;
+            for (NicknameData p : players)
+              if (p.getUuid().equals(playerData.get().getUuid()))
+                isDuplicate = true;
+
+            if (!isDuplicate)
+              players.add(playerData.get());
+          }
         }
+      }
     }
 
-    private void addToCache(NicknameData nicknameData)
-    {
-        this.cachedPlayers.put(nicknameData.getName(), nicknameData.getUuid());
-        if (nicknameData.getNickname().isPresent())
-        {
-            List<UUID> set = this.cachedNicknames.get(nicknameData.getNickname().get());
-            if (set == null)
-            {
-                set = new ArrayList<>();
-                set.add(nicknameData.getUuid());
-                this.cachedNicknames.put(nicknameData.getNickname().get().replaceAll(Reference.COLOUR_AND_STYLE_PATTERN, ""), set);
-            }else if (!set.contains(nicknameData.getUuid()))
-                set.add(nicknameData.getUuid());
-        }
+    return players;
+  }
+
+  @Override
+  public void close() throws IOException {
+
+  }
+
+  private void removePlayerFile(UUID uuid) {
+    File file = new File(playerDir, uuid.toString() + EXTENSION);
+    if (file.exists()) {
+      file.delete();
     }
+  }
 
-    @Override
-    public void savePlayer(NicknameData player)
-    {
-        try
-        {
-            writePlayer(player);
-            addToCache(player);
-
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+  private void writePlayer(NicknameData player) throws IOException {
+    File file = new File(playerDir, player.getUuid().toString() + EXTENSION);
+    if (!file.exists()) {
+      file.createNewFile();
     }
+    JsonWriter writer = new JsonWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
+    writer.setIndent("    ");
+    writePlayer(writer, player);
+    writer.close();
+  }
 
-    @Override
-    public void removePlayer(NicknameData player)
-    {
-        removePlayerFile(player.getUuid());
+  private void writePlayer(JsonWriter writer, NicknameData player) throws IOException {
+    writer.beginObject();
+    writer.name("Last known name").value(player.getName());
+    writer.name("Tokens").value(player.getTokensRemaining());
+    writer.name("Last changed").value(player.getLastChanged());
+    writer.name("Accepted rules").value(player.hasAcceptedRules());
+    if (player.getNickname().isPresent())
+      writer.name("Nickname").value(player.getNickname().get());
+    else
+      writer.name("Nickname").nullValue();
+
+    writer.name("Past nicknames").beginArray();
+
+    for (String nick : player.getPastNicknames())
+      writer.value(nick);
+
+    writer.endArray();
+    writer.endObject();
+  }
+
+  private Optional<NicknameData> readPlayer(UUID uuid) throws IOException {
+    File file = new File(playerDir, uuid.toString() + EXTENSION);
+    if (!file.exists())
+      return Optional.empty();
+
+    try (JsonReader reader = new JsonReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+      return readPlayer(reader, uuid);
     }
+  }
 
-    @Override
-    public Optional<NicknameData> getPlayer(String name)
-    {
-        UUID uuid = this.cachedPlayers.get(name);
-        if (uuid == null)
-            return Optional.empty();
-        else
-            return getPlayer(uuid);
-    }
+  private Optional<NicknameData> readPlayer(JsonReader reader, UUID uuid) throws IOException {
+    reader.beginObject();
+    String playerName = null, nickname = null;
+    int tokens = 0;
+    long lastChanged = 0;
+    boolean acceptedRules = false;
+    List<String> pastNicknames = new ArrayList<>();
 
-    @Override
-    public Optional<NicknameData> getPlayer(UUID uuid)
-    {
-        try
-        {
-            return readPlayer(uuid);
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-            return Optional.empty();
-        }
-    }
-
-    @Override
-    public List<NicknameData> getPlayerByNickname(String nickname, int limit)
-    {
-        List<NicknameData> players = new ArrayList<>();
-
-        for (Map.Entry<String, List<UUID>> entry : this.cachedNicknames.entrySet())
-        {
-            if (entry.getKey().toLowerCase().contains(nickname.toLowerCase()))
-            {
-                for (UUID uuid : entry.getValue())
-                {
-                    Optional<NicknameData> playerData = getPlayer(uuid);
-                    if (playerData.isPresent())
-                    {
-                        boolean isDuplicate = false;
-                        for (NicknameData p : players)
-                            if (p.getUuid().equals(playerData.get().getUuid()))
-                                isDuplicate = true;
-
-                        if (!isDuplicate)
-                            players.add(playerData.get());
-                    }
-                }
-            }
-        }
-
-        return players;
-    }
-
-    @Override
-    public void close() throws IOException
-    {
-
-    }
-
-    private void removePlayerFile(UUID uuid)
-    {
-        File file = new File(playerDir, uuid.toString() + EXTENSION);
-        if (file.exists())
-            file.delete();
-    }
-
-    private void writePlayer(NicknameData player) throws IOException
-    {
-        File file = new File(playerDir, player.getUuid().toString() + EXTENSION);
-        if (!file.exists())
-            file.createNewFile();
-        JsonWriter writer = new JsonWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
-        writer.setIndent("    ");
-        writePlayer(writer, player);
-        writer.close();
-    }
-
-    private void writePlayer(JsonWriter writer, NicknameData player) throws IOException
-    {
-        writer.beginObject();
-        writer.name("Last known name").value(player.getName());
-        writer.name("Tokens").value(player.getTokensRemaining());
-        writer.name("Last changed").value(player.getLastChanged());
-        writer.name("Accepted rules").value(player.hasAcceptedRules());
-        if (player.getNickname().isPresent())
-            writer.name("Nickname").value(player.getNickname().get());
-        else
-            writer.name("Nickname").nullValue();
-
-        writer.name("Past nicknames").beginArray();
-
-        for (String nick : player.getPastNicknames())
-            writer.value(nick);
-
-        writer.endArray();
-        writer.endObject();
-    }
-
-    private Optional<NicknameData> readPlayer(UUID uuid) throws IOException
-    {
-        File file = new File(playerDir, uuid.toString() + EXTENSION);
-        if (!file.exists())
-            return Optional.empty();
-
-        try (JsonReader reader = new JsonReader(new InputStreamReader(new FileInputStream(file), "UTF-8")))
-        {
-            return readPlayer(reader, uuid);
-        }
-    }
-
-    private Optional<NicknameData> readPlayer(JsonReader reader, UUID uuid)throws IOException
-    {
-        reader.beginObject();
-        String playerName = null, nickname = null;
-        int tokens = 0;
-        long lastChanged = 0;
-        boolean acceptedRules = false;
-        List<String> pastNicknames = new ArrayList<>();
+    while (reader.hasNext()) {
+      String name = reader.nextName();
+      if (name.equals("Last known name"))
+        playerName = reader.nextString();
+      else if (name.equals("Nickname") && reader.peek() != JsonToken.NULL)
+        nickname = reader.nextString();
+      else if (name.equals("Tokens"))
+        tokens = reader.nextInt();
+      else if (name.equals("Last changed"))
+        lastChanged = reader.nextLong();
+      else if (name.equals("Accepted rules"))
+        acceptedRules = reader.nextBoolean();
+      else if (name.equals("Past nicknames")) {
+        reader.beginArray();
 
         while (reader.hasNext())
-        {
-            String name = reader.nextName();
-            if (name.equals("Last known name"))
-                playerName = reader.nextString();
-            else if (name.equals("Nickname") && reader.peek() != JsonToken.NULL)
-                nickname = reader.nextString();
-            else if (name.equals("Tokens"))
-                tokens = reader.nextInt();
-            else if (name.equals("Last changed"))
-                lastChanged = reader.nextLong();
-            else if (name.equals("Accepted rules"))
-                acceptedRules = reader.nextBoolean();
-            else if (name.equals("Past nicknames"))
-            {
-                reader.beginArray();
+          pastNicknames.add(reader.nextString());
 
-                while (reader.hasNext())
-                    pastNicknames.add(reader.nextString());
-
-                reader.endArray();
-            }else
-                reader.skipValue();
-        }
-
-        reader.endObject();
-        if (playerName == null)
-            return Optional.empty();
-
-        NicknameData nicknameData = new NicknameData(playerName, uuid);
-
-        nicknameData.setTokensRemaining(tokens);
-        nicknameData.setLastChanged(lastChanged);
-        nicknameData.setAcceptedRules(acceptedRules);
-        if (nickname != null)
-            nicknameData.setNickname(nickname);
-        if (pastNicknames.size() > 0)
-            nicknameData.addPastNickname(pastNicknames);
-
-        return Optional.of(nicknameData);
+        reader.endArray();
+      }
+      else
+        reader.skipValue();
     }
+
+    reader.endObject();
+    if (playerName == null)
+      return Optional.empty();
+
+    NicknameData nicknameData = new NicknameData(playerName, uuid);
+
+    nicknameData.setTokensRemaining(tokens);
+    nicknameData.setLastChanged(lastChanged);
+    nicknameData.setAcceptedRules(acceptedRules);
+    if (nickname != null)
+      nicknameData.setNickname(nickname);
+    if (pastNicknames.size() > 0)
+      nicknameData.addPastNickname(pastNicknames);
+
+    return Optional.of(nicknameData);
+  }
 
 }
