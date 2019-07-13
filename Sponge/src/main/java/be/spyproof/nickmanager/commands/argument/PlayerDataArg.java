95e611cda1514205e2820b57ee4f505dbcc1af53
Nils Yseboodt <nils.yseboodt@gmail.com>
2019-07-13 06:26:30 +0200

    Support UUIDs for sponge players

--- Sponge/src/main/java/be/spyproof/nickmanager/commands/argument/PlayerDataArg.java
+++ Sponge/src/main/java/be/spyproof/nickmanager/commands/argument/PlayerDataArg.java
@@ -15,52 +15,56 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.regex.Pattern;
 
 /**
  * Created by Spyproof on 28/10/2016.
  */
-public class PlayerDataArg extends CommandElement
-{
-    private ISpongeNicknameController playerController;
+public class PlayerDataArg extends CommandElement {
+  private static final Pattern uuidPattern = Pattern.compile("[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}");
+  private ISpongeNicknameController playerController;
 
-    public PlayerDataArg(String key, ISpongeNicknameController playerController)
-    {
-        super(Text.of(key));
-        this.playerController = playerController;
+  public PlayerDataArg(String key, ISpongeNicknameController playerController) {
+    super(Text.of(key));
+    this.playerController = playerController;
+  }
+
+  @Nullable
+  @Override
+  protected Object parseValue(CommandSource source, CommandArgs args) throws ArgumentParseException {
+    String argument = args.next();
+    if (argument == null) {
+      return null;
     }
 
-    @Nullable
-    @Override
-    protected Object parseValue(CommandSource source, CommandArgs args) throws ArgumentParseException
-    {
-        String argument = args.next();
-        Optional<? extends NicknameData> playerData = this.playerController.getPlayer(argument);
-
-        if (playerData.isPresent())
-            return playerData.get();
-        else
-            return null;
+    Optional<? extends NicknameData> playerData;
+    if (uuidPattern.matcher(argument).matches()) {
+      playerData = this.playerController.getPlayer(UUID.fromString(argument));
+    } else {
+      playerData = this.playerController.getPlayer(argument);
     }
 
-    @Override
-    public List<String> complete(CommandSource src, CommandArgs args, CommandContext context)
-    {
-        List<String> names = new ArrayList<>();
-        if (!args.hasNext())
-            return names;
+    return playerData.orElse(null);
+  }
 
-        String peek;
-        try {
-            peek = args.peek().toLowerCase();
-        }
-        catch (ArgumentParseException e) {
-            return names;
-        }
+  @Override
+  public List<String> complete(CommandSource src, CommandArgs args, CommandContext context) {
+    List<String> names = new ArrayList<>();
+    if (!args.hasNext())
+      return names;
 
-        for (Player player : Sponge.getServer().getOnlinePlayers())
-            if (player.getName().toLowerCase().startsWith(peek))
-                names.add(player.getName());
-
-        return names;
+    String peek;
+    try {
+      peek = args.peek().toLowerCase();
+    } catch (ArgumentParseException e) {
+      return names;
     }
+
+    for (Player player : Sponge.getServer().getOnlinePlayers())
+      if (player.getName().toLowerCase().startsWith(peek))
+        names.add(player.getName());
+
+    return names;
+  }
 }
