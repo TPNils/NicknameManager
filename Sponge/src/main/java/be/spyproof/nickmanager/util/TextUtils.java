55f9906667e65c1cf8961ac0dd5b2709a9369123
Nils Yseboodt <nils.yseboodt@gmail.com>
2019-09-17 21:14:24 +0200

    (1.1.2a) Fix: Splitting texts for new line

--- Sponge/src/main/java/be/spyproof/nickmanager/util/TextUtils.java
+++ Sponge/src/main/java/be/spyproof/nickmanager/util/TextUtils.java
@@ -1,11 +1,15 @@
 package be.spyproof.nickmanager.util;
 
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.ClickAction;
+import org.spongepowered.api.text.action.HoverAction;
+import org.spongepowered.api.text.action.ShiftClickAction;
 import org.spongepowered.api.text.format.TextColor;
 import org.spongepowered.api.text.format.TextStyle;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class TextUtils {
@@ -17,27 +21,9 @@ public class TextUtils {
    * @return a list of texts
    */
   public static List<Text> splitLines(Text text) {
-    return splitLines(text, null, null);
-  }
-
-  private static List<Text> splitLines(Text text, TextColor parentColor, TextStyle parentStyle) {
-    List<Text.Builder> splitBuilders = new ArrayList<>();
-
-    TextColor color = processColor(text.getColor(), parentColor);
-    TextStyle style = processStyle(text.getStyle(), parentStyle);
-    Collection<String> plainTexts = separateNewLines(text.toPlainSingle());
-
-    for (String plainText : plainTexts) {
-      splitBuilders.add(Text.builder(plainText).color(color).style(style));
-    }
-
-    Text.Builder lastBuilder = splitBuilders.get(splitBuilders.size()-1);
-    for (Text child : text.getChildren()) {
-      lastBuilder.append(splitLines(child, color, style));
-    }
+    List<Text.Builder> splitBuilders = splitLines(text, 0, null, null, null, null);
 
     List<Text> texts = new ArrayList<>();
-
     for (Text.Builder builder : splitBuilders) {
       texts.add(builder.build());
     }
@@ -45,17 +31,57 @@ public class TextUtils {
     return texts;
   }
 
-  private static TextColor processColor(TextColor selfColor, TextColor parentColor) {
-    return selfColor == null ? parentColor : selfColor;
+  private static List<Text.Builder> splitLines(Text text, int depth, TextStyle parentStyle, HoverAction parentHoverAction, ClickAction parentClickAction, ShiftClickAction parentShiftClickAction) {
+    List<Text.Builder> splitBuilders = new ArrayList<>();
+
+    TextStyle style = processStyle(text.getStyle(), parentStyle);
+    TextColor color = text.getColor();
+    HoverAction hoverAction = text.getHoverAction().orElse(parentHoverAction);
+    ClickAction clickAction = text.getClickAction().orElse(parentClickAction);
+    ShiftClickAction shiftClickAction = text.getShiftClickAction().orElse(parentShiftClickAction);
+    String t = text.toPlainSingle();
+    Collection<String> plainTexts = separateNewLines(text.toPlainSingle());
+
+    for (String plainText : plainTexts) {
+      Text.Builder builder = Text.builder(plainText).color(color).style(style);
+      if (hoverAction != null) {
+        builder.onHover(hoverAction);
+      }
+      if (clickAction != null) {
+        builder.onClick(clickAction);
+      }
+      if (shiftClickAction != null) {
+        builder.onShiftClick(shiftClickAction);
+      }
+      splitBuilders.add(builder);
+    }
+
+    Text.Builder lastBuilder = splitBuilders.get(splitBuilders.size()-1);
+    for (Text child : text.getChildren()) {
+      List<Text.Builder> childSplit = splitLines(child, depth+1, style, hoverAction, clickAction, shiftClickAction);
+      for (int i = 0; i < childSplit.size(); i++) {
+        if (i == 0) {
+          lastBuilder.append(childSplit.get(i).build());
+        } else {
+          lastBuilder = childSplit.get(i).color(color).style(style);
+          splitBuilders.add(lastBuilder);
+        }
+      }
+    }
+
+    return splitBuilders;
   }
 
   private static TextStyle processStyle(TextStyle selfStyle, TextStyle parentStyle) {
     if (selfStyle == null) {
+      if (parentStyle == null) {
+        return new TextStyle(null, null, null, null, null);
+      }
       return parentStyle;
     }
 
     if (parentStyle == null) {
-      return null;
+      return new TextStyle(null, null, null, null, null);
     }
 
     return new TextStyle(
@@ -68,19 +94,24 @@ public class TextUtils {
   }
 
   private static Collection<String> separateNewLines(String text) {
+    if (text == null || text.length() == 0) {
+      return Collections.singleton("");
+    }
+
     List<String> parts = new ArrayList<>();
     int nextNewLine;
-    while (text.contains(NEW_LINE)) {
+    while (text != null && text.contains(NEW_LINE)) {
       nextNewLine = text.indexOf('\n');
       parts.add(text.substring(0, nextNewLine));
+      parts.add("");
       if (text.length() > nextNewLine+1) {
         text = text.substring(nextNewLine+1);
       } else {
-        text = "";
+        text = null;
       }
     }
 
-    if (text.length() > 0) {
+    if (text != null) {
       parts.add(text);
     }
 
